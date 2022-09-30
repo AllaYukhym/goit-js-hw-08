3c26d0006e6e10207e9750eae70dfdd2d05d2686
AllaYukhym <yukhymenko.alla@gmail.com>
2022-09-30 21:51:52 +0200

    add check for localstorage emptyness

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,8 +6,9 @@ const LOCALE_STORAGE_KEY = 'videoplayer-current-time';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-const throttledOnFormInput = throttle(saveCurrentData, 1000);
+initPage();
 
+const throttledOnFormInput = throttle(saveCurrentData, 1000);
 player.on('timeupdate', throttledOnFormInput);
 
 function saveCurrentData(data) {
@@ -15,4 +16,10 @@ function saveCurrentData(data) {
   save(LOCALE_STORAGE_KEY, data.seconds);
 }
 
-player.setCurrentTime(load(LOCALE_STORAGE_KEY));
+function initPage() {
+  const saveData = load(LOCALE_STORAGE_KEY);
+  if (!saveData) {
+    return;
+  }
+  player.setCurrentTime(load(LOCALE_STORAGE_KEY));
+}
