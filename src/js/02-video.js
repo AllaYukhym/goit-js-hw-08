120cb6b46dcbba5d33bff9a913f30fb95bba0204
AllaYukhym <yukhymenko.alla@gmail.com>
2022-09-30 12:03:56 +0200

    import @vimeo/player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,18 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+import { save, load } from './storage';
 
+const LOCALE_STORAGE_KEY = 'videoplayer-current-time';
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
-player.on('play', function () {
-  console.log('played the video!');
-});
+const throttledOnFormInput = throttle(saveCurrentData, 1000);
 
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
+player.on('timeupdate', throttledOnFormInput);
+
+function saveCurrentData(data) {
+  console.log(data.seconds);
+  save(LOCALE_STORAGE_KEY, data.seconds);
+}
+
+player.setCurrentTime(load(LOCALE_STORAGE_KEY));
